95562033131eaeaab8e64e6aea0dfc9c160787d0
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2022-12-14 16:12:35 -0300

    Adiciona rota get que retorna todas as pessoas palestrantes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const path = require('path');
+const { read } = require('./utils/functionsFS');
+
+const filePath = path.resolve('src', 'talker.json');
 
 const app = express();
 app.use(express.json());
+
+app.get('/talker', async (req, res) => {
+  const data = await read(filePath);
+  if (!data) return res.status(500).send('Erro na leitura do banco de dados');
+  return res.status(200).json(data);
+});
+
+/* ============================= */
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
