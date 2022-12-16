b719e38c0851cf566e986ef86b8f2fedf95b6e55
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2022-12-15 22:04:41 -0300

    fix: middleware validateLogin cobre todas as regras de negocio

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -1,19 +1,15 @@
-const validar = (email, password, res) => {
-  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  if (password.length < 6) { 
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
-  }
-};
-
 const validateLoginMiddlewares = (req, res, next) => {
   const { email, password } = req.body;
-  validar(email, password, res);
-  if (!email.includes('@gmail.com') || !email.includes('@email.com')) {
-    return res
-      .status(400)
-      .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
-  }
+  const ifs = [
+  { message: 'O campo "email" é obrigatório', conditional: !email },
+  { message: 'O campo "password" é obrigatório', conditional: !password },
+  { message: 'O "email" deve ter o formato "email@email.com"', 
+    conditional: email ? !(email.includes('@gmail.com') || email.includes('@email.com')) : false },
+  { message: 'O "password" deve ter pelo menos 6 caracteres', 
+    conditional: (password ? (password.length < 6) : false) },
+  ];
+  const cond = ifs.find(({ conditional }) => conditional);
+  if (cond) return res.status(400).json({ message: cond.message });
   next();
 };
 
