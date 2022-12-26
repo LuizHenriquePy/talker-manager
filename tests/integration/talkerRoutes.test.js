1145689bb9ce280715a3bee8f82211cf1c49580d
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2022-12-26 18:54:36 -0300

    test: get /talker/:id retorna menssagem: palestrante nao encontrado

--- tests/integration/talkerRoutes.test.js
+++ tests/integration/talkerRoutes.test.js
@@ -125,11 +125,15 @@ describe('routes talker', function() {
     });
   });
   describe('get /talker/:id', function() {
-     it('return person not found', async function() {
+    it('return person not found', async function() {
+      const response = await chai.request(app).get('/talker/34');
 
-     });
-     it('return a person', async function() {
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.haveOwnProperty('message');
+      expect(response.body.message).to.be.equal('Pessoa palestrante não encontrada');
+    });
+    it('return a person', async function() {
 
-     });
+    });
   });
 });
