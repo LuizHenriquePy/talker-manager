2aa7be651d55986b53eb956be5820225745f115a
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-01-02 14:58:53 -0300

    test: adiciona testes de unidade para funcao createToken

---
+++ tests/unit/createToken.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const createToken = require('../../src/utils/createToken');
+
+describe('Function createToken', function () {
+  it('returns a token with 16 characters', function () {
+    const token = createToken();
+    console.log(typeof token);
+    expect(token).to.be.a('string');
+    expect(token).to.have.lengthOf(16);
+  });
+});
