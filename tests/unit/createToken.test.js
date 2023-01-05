0bd5c6544fb94d40611c6aa202e3b630a44d294d
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-01-05 06:27:05 -0300

    test: remove console.log desnecessario

--- tests/unit/createToken.test.js
+++ tests/unit/createToken.test.js
@@ -4,7 +4,6 @@ const createToken = require('../../src/utils/createToken');
 describe('Function createToken', function () {
   it('returns a token with 16 characters', function () {
     const token = createToken();
-    console.log(typeof token);
     expect(token).to.be.a('string');
     expect(token).to.have.lengthOf(16);
   });
